2d819eb52f9c2c8577f461c57fb745012411a9d8
KunitsynaDaria <115484233+KunitsynaDaria@users.noreply.github.com>
2022-11-13 11:29:28 +0300

    Update task1.py

--- lab5/task1.py
+++ lab5/task1.py
@@ -1,7 +1,9 @@
 from pprint import pprint
 
 
-list_numbers = [[bin(i) for i in range(16)], [i for i in range(16)], [hex(i) for i in range(16)], [oct(i) for i in range(16)]]
-list_systems = ["bin", "dec", "hex", "oct"]
-list_dicts = [{list_systems[i]: list_numbers[i][j] for i in range(4)} for j in range(16)]
+#list_numbers = [[bin(i) for i in range(16)], [i for i in range(16)], [hex(i) for i in range(16)], [oct(i) for i in range(16)]]
+#list_systems = ["bin", "dec", "hex", "oct"]
+#list_dicts = [{list_systems[i]: list_numbers[i][j] for i in range(4)} for j in range(16)] - создание словаря тоже через  list comprehension
+
+list_dicts = [{'bin': bin(i), 'dec': i, 'oct': oct(i), 'hex': hex(i)} for i in range(16)] # - так покороче
 pprint(list_dicts)
